069d14d67c836e9b4ebfd5071e040666795334ae
Nils <nils@fieryaura.eu>
2024-11-13 19:27:34 +0100

    clean

--- obfs.sh
+++
@@ -1,9 +0,0 @@
-pyarmor obfuscate --recursive \
-    --output dist \
-    --exclude .venv* \
-    --exclude __pycache__* \
-    --exclude .git* \
-    --exclude .idea* \
-    --exclude *.sh \
-    --exact \
-    main.py autocorrect_pro/*.py
